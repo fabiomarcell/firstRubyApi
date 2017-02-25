13d54311d1a666c0ba9fc73774b375913aefbe93
Fabio Marcell <fabio.marcell@outlook.com>
2017-02-24 21:44:18 -0300

    Test sql database

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'sinatra'
 require 'httparty'
 require 'active_record'
+require 'activerecord-sqlserver-adapter'
 require 'tiny_tds'
 require 'json'
 
@@ -15,4 +16,15 @@ end
 
 get '/' do
   [{v1: "Foo"},{v1: "Bar"}].to_json
+end
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlserver",
+  :host => "tcp:checklistrb.database.windows.net",
+  :username => "sa123",
+  :password => "2684Fbi6",
+  :database => "checklist")
+
+class Tarefa < ActiveRecord::Base
+   set_table_name "tblTarefa"
 end
